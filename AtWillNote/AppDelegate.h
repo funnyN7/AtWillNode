08739ba2bebc48c3872c69c2a0d7e016c8d1dfce
姚琪 <yaoqi@yaoqideMacBook-Pro.local>
2015-12-14 23:21:10 +0800

    first Commit

---
+++ AtWillNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AtWillNote
+//
+//  Created by 姚琪 on 15/12/14.
+//  Copyright © 2015年 yaoqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AtWillNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AtWillNote
+//
+//  Created by 姚琪 on 15/12/14.
+//  Copyright © 2015年 yaoqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
